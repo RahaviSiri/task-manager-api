5e59f75e7f31f400737cab2c02ed810daa7f11a1
Rahavi Sirithar <RahaviS@99x.io>
2026-01-16 07:20:34 +0530

    Add project files.

---
+++ task-manager-api/Config/MongoDbSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace task_manager_api.Config
+{
+    public class MongoDbSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string CollectionName { get; set; }
+    }
+}
---
+++ task-manager-api/Program.cs
@@ -0,0 +1,39 @@
+using task_manager_api.Config;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+// Register database settings
+builder.Services.Configure<MongoDbSettings>(
+    builder.Configuration.GetSection("Mongodb"));
+
+// Add Cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder => builder.AllowAnyOrigin()
+                          .AllowAnyHeader()
+                          .AllowAnyMethod());
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.UseCors("AllowAll");
+
+app.Run();
